10ddf4e81bf51fc9c5b0ce74705720aff83af9b2
b0uwe322 <dima.ermolaev.98@bk.ru>
2026-02-22 18:41:53 +0300

    Update LB1.py and test_LB1.py

--- test_LB1.py
+++ test_LB1.py
@@ -7,7 +7,7 @@ class TestCircleArea(unittest.TestCase):
 
     def test_area(self):
         self.assertEqual(circle_area(3), pi * 3 ** 2)
-        self.assertEqual(circle_area(1), 0)
+        self.assertEqual(circle_area(1), pi)
         self.assertEqual(circle_area(0), 0)
         self.assertEqual(circle_area(2.5), pi * 2.5 ** 2)
 
