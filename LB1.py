a768e1e7525d0185cfe784cc63e7432c3324b621
b0uwe322 <dima.ermolaev.98@bk.ru>
2026-02-22 18:20:47 +0300

    Update LB1.py and test_LB1.py

--- LB1.py
+++ LB1.py
@@ -1,12 +1,16 @@
 from math import pi
 
+
 def circle_area(r):
+    if r < 0:
+        raise ValueError("Значение радиуса должно быть > 0")
+    if type(r) not in (float, int):
+        raise TypeError("Тип данных должен быть int или float!")
     return pi*r**2
 
 
-
 if __name__ == "__main__":
-    r_list = [2, 0, -14, True, [24], 'ergeg']
+    r_list = [2, 0, -14, True, [24], 'string']
     for i in r_list:
         res = circle_area(i)
         print(f"Исходные данные: {i} Результат: {res}")
--- test_LB1.py
+++ test_LB1.py
@@ -2,6 +2,7 @@ import unittest
 from LB1 import circle_area
 from math import pi
 
+
 class TestCircleArea(unittest.TestCase):
 
     def test_area(self):
@@ -9,3 +10,11 @@ class TestCircleArea(unittest.TestCase):
         self.assertEqual(circle_area(1), pi)
         self.assertEqual(circle_area(0), 0)
         self.assertEqual(circle_area(2.5), pi * 2.5 ** 2)
+
+    def test_values(self):
+        self.assertRaises(ValueError, circle_area, -14)
+
+    def test_type_date(self):
+        self.assertRaises(TypeError, circle_area, [24])
+        self.assertRaises(TypeError, circle_area, "string")
+        self.assertRaises(TypeError, circle_area, True)
